855019f306c244e5412970f5c55b8f6eda6da048
Andres Scarafia <andres.g.scarafia@gmail.com>
2022-05-29 10:43:13 -0300

    Procesamiento datos museos

--- procesamiento_datos.py
+++ procesamiento_datos.py
@@ -1,24 +1,23 @@
 #procesamiento_datos.py
 import pandas as pd
 import numpy as np
-#import os
 
-data_cines = pd.read_csv('cines/2022-mayo/cines-26-05-2022.csv')
-data_museos = pd.read_csv('museos/2022-mayo/museos-26-05-2022.csv')
-data_bibliotecas = pd.read_csv('bibliotecas/2022-mayo/bibliotecas-26-05-2022.csv')
+data_cines = pd.read_csv('cines/2022-mayo/cines-28-05-2022.csv')
+data_museos = pd.read_csv('museos/2022-mayo/museos-28-05-2022.csv')
+data_bibliotecas = pd.read_csv('bibliotecas/2022-mayo/bibliotecas-28-05-2022.csv')
 
 nombres_columnas = ['cod_localidad',
-				 'id_provincia',
-				 'id_departamento',
-				 'categoría',
-				 'provincia',
-				 'localidad',
-				 'nombre',
-				 'domicilio',
-				 'código postal',
-				 'número de teléfono',
-				 'mail',
-				 'web']
+				 	'id_provincia',
+				 	'id_departamento',
+					'categoría',
+				 	'provincia',
+				 	'localidad',
+				 	'nombre',
+				 	'domicilio',
+				 	'código postal',
+				 	'número de teléfono',
+				 	'mail',
+				 	'web']
 
 data_cines.rename(columns = {'Cod_Loc': 'cod_localidad',
 						 	 'IdProvincia': 'id_provincia',
@@ -33,19 +32,33 @@ data_cines.rename(columns = {'Cod_Loc': 'cod_localidad',
 						 	 'Mail': 'mail',
 						 	 'Web': 'web'}, inplace = True)
 
-data_cines['número de teléfono'] = np.where(data_cines.cod_area == 's/d',data_cines['número de teléfono'], data_cines.cod_area + data_cines['número de teléfono'])
+data_cines['número de teléfono'] = np.where(data_cines.cod_area == 's/d',
+											data_cines['número de teléfono'],
+											data_cines.cod_area + ' ' + data_cines['número de teléfono'])
 
-data_cines.domicilio = np.where(data_cines.Piso == 's/d', data_cines.domicilio, data_cines.domicilio + ' ' + data_cines.Piso)
+data_cines.domicilio = np.where(data_cines.Piso == 's/d',
+								data_cines.domicilio, 
+								data_cines.domicilio + ' ' + data_cines.Piso)
 
 for column in data_cines.columns:
 	if column not in nombres_columnas:
 		del data_cines[column]
 
 
+data_museos.rename(columns = {'Cod_Loc': 'cod_localidad',
+						 	 'IdProvincia': 'id_provincia',
+						 	 'IdDepartamento': 'id_departamento',
+						 	 'categoria': 'categoría', 
+						 	 'direccion': 'domicilio',
+							  'CP': 'código postal',
+						 	 'telefono': 'número de teléfono',
+						 	 'Mail': 'mail',
+						 	 'Web': 'web'}, inplace = True)
 
-#Selecciono codigo de area y telefono
-#data_cines_tel = data_cines[['cod_area', 'número de teléfono']]
+data_museos['número de teléfono'] = np.where(data_museos.cod_area.notnull() & data_museos['número de teléfono'].notnull(),
+											 data_museos.cod_area.astype('Int64').astype(str) + ' ' + data_museos['número de teléfono'],
+											 data_museos['número de teléfono'])
 
-#Selecciono las que tienen solo datos validos
-#data_cines_tel = data_cines_tel[(data_cines_tel['cod_area'] != 's/d') &
-#			                    (data_cines_tel['número de teléfono'] != 's/d')]
+for column in data_museos.columns:
+	if column not in nombres_columnas:
+		del data_museos[column]
